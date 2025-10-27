4d6fcf3e1faad869da367b65451b4c8294835c48
Christian <christianroca321@gmail.com>
2025-10-27 16:41:43 -0600

    Correccion

--- app/Http/Controllers/Auth/AccesoController.php
+++ app/Http/Controllers/Auth/AccesoController.php
@@ -15,45 +15,47 @@ class AccesoController extends Controller
     }
     public function iniciarSesion(Request $request){
         $validator = Validator::make($request->all(), [
-            'email' => 'required|email|max:255' ,
-            'password' => 'required|string|min:6' 
+            'email' => 'required|email|max:255',
+            'password' => 'required|string|min:6'
         ],[
-            'email.required' => 'El correo electronico es obligatorio',
-            'email.email' => 'Debe ingresar un correo electronico valido',
-            'email.max' => 'El correo electronico no puede tener mas de 255 caracteres',
-            'password.required' => 'La contraseña debe ser una cadena de texto',
+            'email.required' => 'El correo electrónico es obligatorio',
+            'email.email' => 'Debe ingresar un correo electrónico válido',
+            'email.max' => 'El correo electrónico no puede tener más de 255 caracteres',
+            'password.required' => 'La contraseña es obligatoria',
             'password.string' => 'La contraseña debe ser una cadena de texto',
-            'password.min' => 'La contraseña debe tener al menos 6 caracteres',
+            'password.min' => 'La contraseña debe tener al menos 6 caractéres',
         ]);
 
         if( $validator->fails() ){
             if( $request->ajax() ){
                 return response()->json([
                     'success' => false,
-                    'erros' => $validator->errors()
+                    'errors' => $validator->errors()
                 ], 422);
             }
-            return redirect()
-        -back()
-        ->withErrors( $validator )
-        ->withInput($request->only('email') );
 
+            return redirect()
+            ->back()
+            ->withErrors( $validator )
+            ->withInput( $request->only('email') );
         }
 
         $credenciales = $request->only('email', 'password');
 
         if( Auth::attempt( $credenciales ) ){
-            $request->sesion()->regenerate();
+            $request->session()->regenerate();
             $usuario = Auth::user();
 
             if( $request->ajax() ){
                 return response()->json([
                     'success' => true,
-                    'message' => 'Inicio de sesion exitoso',
+                    'message' => 'Inicio de sesión exitoso',
                     'redirect' => ''
                 ]);
             }
         }
 
+
+        
     }
 }
